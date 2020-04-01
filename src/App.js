d3d9c0aabf0e8cb0f263ce3a7575c69d87f11fb3
victor <v.prada@udla.edu.co>
2020-04-01 02:06:01 -0500

    feat: add funtion App

--- src/App.js
+++ src/App.js
@@ -1,80 +1,23 @@
-import React from 'react';
+import  React, {Component} from  'react';
 import './App.css';
+import task from './sample/task.json';
 
 
-/*function Helloworld() {
-  return (
-    <div id="id"><h1>Hello world</h1> </div>
-  )
-}*/
-
-/*function Helloworld(props) {
-  return (
-    <div id="id">
-      <h1>
-        {props.title}
-      </h1>
-      <h3>
-        {props.subtitle}
-      </h3>
-    </div>
-  )
-}*/
-
-class Helloworld extends React.Component {
+class App extends Component {
 
   state = {
-    show: true
-  }
-
-  togle = () => {
-    this.setState({ show: !this.state.show })
+    task: task
   }
 
   render() {
-    if (this.state.show) {
-      return (
-        <div id="id">
-          <h1>
-            {this.props.title}
-          </h1>
-          <h3>
-            {this.props.subtitle}
-          </h3>
-          <button onClick={this.togle}>
-            Cambio
-          </button>
-        </div>
-      )
-
-    } else {
-      return <div id="id">
-        <h1>
-          no hay elementos
-        </h1>
-        <button onClick={this.togle}>
-          Cambio
-          </button>
-      </div>
-  
-
-    }
-
-  }
-
-}
-
-
-function App() {
-  return (
-    <div>
-      esto es un componente:
-      <Helloworld title="perro" subtitle="el negger ama a fay" />
-      <Helloworld title="perro" subtitle="el negger ama a fay" />
-      <Helloworld title="perro" subtitle="el negger ama a fay" />
-      <Helloworld title="perro" subtitle="el negger ama a fay" />
+    return <div>
+      {this.state.task.map(e => <p key={e.id}>
+        <h1>Titulo:{e.title} </h1>
+        <h2>Id: {e.id}</h2>
+        <p>{e.description}</p>
+      </p>)}
     </div>
-  );
+  }
 }
 
 export default App;
